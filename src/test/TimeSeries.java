0f100f0e3c0f04710e61c6000d3298e385d4e0ed
Daniellez24 <daniellez1123@gmail.com>
2021-11-14 17:51:39 +0200

    update TimeSeries

--- src/test/TimeSeries.java
+++ src/test/TimeSeries.java
@@ -9,23 +9,44 @@ import java.util.Arrays;
 public class TimeSeries {
 
 	String fileName;
-	String[] flightParameters;
+	String[][] dataMatrix;
+
 
 	public TimeSeries(String csvFileName) {
 		fileName = csvFileName;
 	}
 
 	public void readCsvFile() throws Exception {
+		boolean isRead = false;
+		int numOfCriteria = 0;
+		int counter = 1; // represents the lines in the matrix dataNames
+		String[] data;
 		String delimiter = ",";
 		BufferedReader buf = new BufferedReader(new FileReader((this.fileName)));
 		String line;
+
+		line = buf.readLine();
+		if (line!= null){
+			data = line.split(delimiter);
+			numOfCriteria = data.length;
+			dataMatrix = new String[200][numOfCriteria];
+			for (int i = 0; i < numOfCriteria; i++) {
+				dataMatrix[0][i] = data[i];
+			}
+		}
+
+
 		while((line = buf.readLine()) != null){
-			String[] data = line.split(delimiter);
-			if(flightParameters == null)
-				flightParameters = data;
+			data = line.split(delimiter);
+
+			for (int i=0; i<numOfCriteria; i++){
+				dataMatrix[counter][i] = data[i];
+			}
 
 			System.out.println(Arrays.toString(data));
+			counter++;
 		}
+
 		buf.close();
 	}
 	
