d60fc4a1c43278eed01ce7b8ba0f2950379942d0
Daniellez24 <daniellez1123@gmail.com>
2021-11-14 18:28:00 +0200

    fixed TimeSeries

--- src/test/TimeSeries.java
+++ src/test/TimeSeries.java
@@ -9,7 +9,6 @@ import java.util.Arrays;
 public class TimeSeries {
 
 	String fileName;
-	String[][] dataMatrix;
 
 
 	public TimeSeries(String csvFileName) {
@@ -17,37 +16,44 @@ public class TimeSeries {
 	}
 
 	public void readCsvFile() throws Exception {
-		boolean isRead = false;
-		int numOfCriteria = 0;
+		String[][] dataMatrix = new String[200][];
+		int numOfColumns = 0;
 		int counter = 1; // represents the lines in the matrix dataNames
 		String[] data;
 		String delimiter = ",";
 		BufferedReader buf = new BufferedReader(new FileReader((this.fileName)));
 		String line;
+		int numOfLines = getNumOfLines(fileName);
 
 		line = buf.readLine();
 		if (line!= null){
 			data = line.split(delimiter);
-			numOfCriteria = data.length;
-			dataMatrix = new String[200][numOfCriteria];
-			for (int i = 0; i < numOfCriteria; i++) {
+			numOfColumns = data.length;
+			dataMatrix = new String[numOfLines][numOfColumns];
+			for (int i = 0; i < numOfColumns; i++) {
 				dataMatrix[0][i] = data[i];
 			}
 		}
 
-
 		while((line = buf.readLine()) != null){
 			data = line.split(delimiter);
-
-			for (int i=0; i<numOfCriteria; i++){
+			for (int i=0; i<numOfColumns; i++){
 				dataMatrix[counter][i] = data[i];
 			}
-
-			System.out.println(Arrays.toString(data));
 			counter++;
 		}
+		System.out.println(Arrays.toString(dataMatrix));
 
 		buf.close();
+	}
+
+	public int getNumOfLines (String fileName) throws Exception{
+		BufferedReader buf = new BufferedReader(new FileReader((this.fileName)));
+		int counter = 0; // the function will return one line more than reality
+		while((buf.readLine()) != null){
+			counter++;
+		}
+		return counter;
 	}
 	
 }
