ce6afa06827dbf034341fd82dfee0d6b7a40274e
Daniellez24 <daniellez1123@gmail.com>
2022-01-02 15:40:13 +0200

    upload more files to exe4

---
+++ src/test/AnomalyDetectionHandler.java
@@ -0,0 +1,14 @@
+package test;
+
+
+import test.Commands.DefaultIO;
+import test.Server.ClientHandler;
+
+public class AnomalyDetectionHandler implements ClientHandler{
+
+	public class SocketIO implements DefaultIO{
+
+	}
+
+
+}
